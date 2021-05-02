98f00135952cffe6f0ec0a1da9a7b5f8378825bb
LinusBostrom <57948534+Linus-Cson-Ferdinand-Bostrom@users.noreply.github.com>
2021-05-02 21:46:46 +0200

    Hello world?

--- app.rb
+++ app.rb
@@ -5,18 +5,22 @@ require 'bcrypt'
 
 enable :sessions
 
+get('/') do
+  slim(:index)
+end
+
 get('/register') do
   slim(:register)
 end
 
-get('/') do
+get('/login') do
   slim(:login)
 end
 
 post('/login') do
   username = params[:username]
   password = params[:password]
-  db = SQLite3::Database.new('db/todo2021.db')
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
   db.results_as_hash = true
   result = db.execute("SELECT * FROM users WHERE username = ?",username).first
   pwdigest = result["pwdigest"]
@@ -24,59 +28,60 @@ post('/login') do
   
   if BCrypt::Password.new(pwdigest) == password
     session[:id] = id
-    redirect('/todos')
+    redirect('/lists')
   else
-    "FEL LÖSENORD!"
+    "Wrong password!"
   end
-end
-
-post('/todos/:id/delete') do
-  id = params[:id].to_i
-  db = SQLite3::Database.new('db/todo2021.db')
-  db.execute("DELETE FROM todos WHERE id = ?",id)
-  redirect("/todos")
-end
-
-post('/todos/:id/edit') do
-  id = params[:id].to_i
-  todo = params[:todo]
-  db = SQLite3::Database.new('db/todo2021.db')
-  db.results_as_hash = true
-  result = db.execute("UPDATE todos SET content=? WHERE id=?",todo,id).first
-  redirect('/todos')
-end
-
-post('/todos') do
-  id = session[:id]
-  todo = params[:todo]
-  db = SQLite3::Database.new('db/todo2021.db')
-  db.results_as_hash = true
-  result = db.execute("INSERT INTO todos (content,user_id) VALUES (?,?)",todo,id).first
-  redirect('/todos')
-end
-
-get('/todos') do
-  id = session[:id].to_i
-  db = SQLite3::Database.new('db/todo2021.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
-  p "Alla todos från result #{result}"
-  slim(:"todos/index",locals:{todos:result})
 end
 
 post("/users/new") do
   username = params[:username]
   password = params[:password]
   password_confirm = params[:password_confirm]
+  if username != "" && password != "" && password_confirm != ""
+    if password == password_confirm
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/slutprojektWSP21.db')
+      db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+      redirect('/lists')
 
-  if password == password_confirm
-    password_digest = BCrypt::Password.create(password)
-    db = SQLite3::Database.new('db/todo2021.db')
-    db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
-    redirect('/')
-
+    else
+      "Passwords did not match!"
+    end
   else
-    "Passwords did not match!"
+    "Please fill all the boxes."
   end
 end
 
+get('/lists') do
+  id = session[:id].to_i
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM lists WHERE user_id = ?",id)
+  slim(:"lists",locals:{lists:result})
+end
+
+post('/lists') do
+  id = session[:id]
+  list = params[:list]
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.results_as_hash = true
+  result = db.execute("INSERT INTO lists (content,user_id) VALUES (?,?)",list,id).first
+  redirect('/lists')
+end
+
+post('/lists/:id/edit') do
+  id = params[:id].to_i
+  list = params[:list]
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.results_as_hash = true
+  result = db.execute("UPDATE lists SET content=? WHERE id=?",list,id).first
+  redirect('/lists')
+end
+
+post('/lists/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.execute("DELETE FROM lists WHERE id = ?",id)
+  redirect("/lists")
+end
